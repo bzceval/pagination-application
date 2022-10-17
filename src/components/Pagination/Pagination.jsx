a38f8ca416319b1f8a7ba7dbb1523549e8932259
bzceval <busraaceval@gmail.com>
2022-10-17 15:29:54 +0300

    done paginate number

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+
+const Pagination = ({ usersPerPage, totalUsers, paginate }) => {
+
+  const [currentButton, setCurrentButton] = useState(1)
+
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
+    pageNumbers.push(i)
+  }
 
 
-const Pagination = () => {
   return (
-    <div></div>
+    <div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination justify-content-center ">
+          {pageNumbers.map(number => (
+            <li onClick={() => paginate(number)} key={number} className="page-item"> <a href="!#" className="page-link">{number}</a></li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   )
 }
 
 export default Pagination
+
+{/* <li className={`${currentButton === 1 ? "page-item disabled" : "page-item"}`}><a className="page-link" href="#!" onClick={() => setCurrentButton((prev) => prev === 1 ? prev : prev - 1)}>Previous</a></li>
+          {pageNumbers.map((page, index) => {
+             return(<li key={index} className={`${currentButton === page ? "page-item active" : "page-item"}`}><a className="page-link" href="#!">{page}</a></li>)
+          })}
+          <li className={`${currentButton === pageNumbers.length ? "page-item disabled" : "page-item"}`}><a className="page-link" href="#!" onClick={() => setCurrentButton((prev) => prev === 1 ? prev : prev + 1)}>Next</a></li> */}
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,25 @@
 import UserCards from '../UserCards/UserCards'
 import data from "../../utils/data"
 import { useState } from 'react'
-
+import Pagination from "../Pagination/Pagination"
 
 const Users = () => {
     const [currentPage, setCurrentPage] = useState(1)
-    const [usersPerPage] = useState(3)
+    const [usersPerPage] = useState(2)
 
     //Get current user
     const indexOfLastUser = currentPage * usersPerPage;
     const indexOfFirstUser = indexOfLastUser - usersPerPage;
     const currentUsers = data.slice(indexOfFirstUser, indexOfLastUser)
 
+    //Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
 
     return (
         <div>
             {currentUsers.map((currentUsers, id) => <UserCards key={id} data={currentUsers} />)}
-
+            <Pagination usersPerPage={usersPerPage} totalUsers={data.length} paginate={paginate}/>
         </div>
     )
 }
